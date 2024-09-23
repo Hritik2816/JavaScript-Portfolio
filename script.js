f9c8e8706e24b5cada2e7226950858a8b865cb4c
Hritik2816 <133709349+Hritik2816@users.noreply.github.com>
2024-09-24 00:26:13 +0530

    nav and herosec

---
+++ script.js
@@ -0,0 +1,8 @@
+let navH1 = document.querySelector("nav-h1")
+let body = document.querySelector("body")
+navH1.addEventListener("mouseover", function () {
+
+  body.style.display = "none"
+  body.style.backgroundColor = "black"
+
+})
